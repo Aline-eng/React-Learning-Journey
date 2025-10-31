5e2318d2036f8191e3b56993c0b304f40179a462
Aline-eng <alinenzikwinkunda@gmail.com>
2025-10-31 18:54:27 +0200

    Started on the Movie Page Project

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,9 +1,11 @@
+import "./App.css";
+
 function App() {
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to React, Karlie 👋</h1>
-      <p>This is your first React app using Vite.</p>
-    </div>
+    <>
+      
+    </>
   );
 }
 
